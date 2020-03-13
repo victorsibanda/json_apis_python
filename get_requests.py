3254c9f706bc10b8615d5f13b767e807b6eacf52
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-13 16:57:02 +0000

    use of GET

--- get_requests.py
+++ get_requests.py
@@ -1,9 +1,30 @@
 import requests
 import urllib.parse
+import json
 
 # URL = path =arguments
+# Headers and metadata
 # GET REQUESTS
 # Postcodes.io  ---> Follow Documentation
-# Capture the input ----> decrypt HJaosn into dictionarry
+# Capture the input ----> decrypt Jason into dictionary
 
-path = 
+path = "https://postcodes.io/postcodes/"
+argument = 'SE172HY'
+url = path + argument
+
+resq = requests.get(url)
+# print (resq)
+# print (resq.status_code)
+#print(resq.headers)
+# print(resq.content)
+
+#JSON decoder
+
+# print(resq.json())
+# print(resq.json().keys())
+# print(resq.json()['result'])
+# output = resq.json()['result']
+# print(output['european_electoral_region'])
+# print(output['outcode'])
+# print(output['longitude'])
+# print(output['latitude'])
